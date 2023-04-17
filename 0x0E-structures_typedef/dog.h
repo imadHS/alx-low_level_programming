1aab42dbe4eb1cc97b2291fa64a621267c539f22
imad ELHASSBI <78317253+imadHS@users.noreply.github.com>
2023-04-17 10:47:34 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,13 +2,7 @@
 #define DOG
 
 /**
- * struct dog - dog class
- *
- * @name: Name of the dog
- *
- * @age: Age of the dog
- *
- * @owner: Owner of the dog
+and the dog go how how
  */
 
 struct dog
