32ca772dabda96d49cbab1e7598b37417fc68491
imad ELHASSBI <78317253+imadHS@users.noreply.github.com>
2023-05-04 11:25:31 +0100

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,10 +1,4 @@
 #include "main.h"
-/**
- * _power - calculate (base and power)
- * @base: base of the exponet
- * @pow: power of the exponet
- * Return: value of base and power
- */
 unsigned long int _power(unsigned int base, unsigned int pow)
 {
 	unsigned long int num;
@@ -15,11 +9,6 @@ unsigned long int _power(unsigned int base, unsigned int pow)
 		num *= base;
 	return (num);
 }
-/**
- * print_binary - prints the binary representation of a number
- * @n: num of prented
- * Return: void
- */
 void print_binary(unsigned long int n)
 {
 	unsigned long int dev, result;
